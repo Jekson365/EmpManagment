45e2eb1d31ce5bf708bf0f6fff35d6e2b1fdd493
Jeko <jeko.erg@gamil.com>
2025-02-11 14:31:00 +0400

    cc

---
+++ Models/Task/Task.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApp.Models.Task
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+    }
+}
